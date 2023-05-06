11dc4c9496d4ee69eb177dd4f856c41e9d00c49c
githubproxyh <129563090+githubproxyh@users.noreply.github.com>
2023-05-07 07:17:16 +0800

    Update douyu.php

--- src/douyu.php
+++ src/douyu.php
@@ -1,10 +1,10 @@
 <?php
 $id = $_GET[ 'id' ];
 	/** 
-    $bstrURL = 'https://hls1a-akm.douyucdn.cn/api/nc/stream/roomPlayer';
+    $bstrURL = 'https://dyp2p-hw.douyucdn.cn/api/nc/stream/roomPlayer';
     $postData = "room_id=$id&big_ct=cpn-androidmpro&did=10000000000000000000000000001501&mt=2&rate=1";
 	**/
-$bstrURL = 'https://hls1a-akm.douyucdn.cn/Livenc/Getplayer/newRoomPlayer';
+$bstrURL = 'https://dyp2p-hw.douyucdn.cn/Livenc/Getplayer/newRoomPlayer';
 $postData = 'room_id=' . $id . '&token=wxapp&rate=1&did=10000000000000000000000000001501&big_ct=cpn-androidinpro&is_Mix=false';
 $ch = curl_init();
 curl_setopt( $ch, CURLOPT_URL, $bstrURL );
