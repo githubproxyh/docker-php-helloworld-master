e8361bea2afea422be9b4e533faaa6ff7f1d4dfc
githubproxyh <129563090+githubproxyh@users.noreply.github.com>
2023-06-18 15:32:22 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,6 +2,6 @@
 
 echo "欢迎来到我的世界! <br>";
 echo "Hello World! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+echo '<img src="https://php.chinaiptv.eu.org/000.png">';
 
 ?>
